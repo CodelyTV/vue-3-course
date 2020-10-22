36710c05bc988bdf0b948d5354df9965c7348462
Nuria <soriano.nuria@gmail.com>
2020-10-22 09:04:44 +0200

    add 4-1-changes-in-style lesson start

---
+++ 4-1-changes-in-style/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <CourseDetail />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import CourseDetail from "@/components/CourseDetail.vue";
+
+export default defineComponent({
+  components: {
+    CourseDetail,
+  },
+});
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ 4-1-changes-in-style/src/components/CourseDetail.vue
@@ -0,0 +1,73 @@
+<template>
+  <header
+    class="header"
+    :style="
+      `background-image: ${background}; background-color: ${course.color}`
+    "
+  >
+    <img :src="course.thumbnail" alt="" />
+    <div class="header-info">
+      <h1>{{ course.title }}</h1>
+      <small>{{ course.authors.join(", ") }}</small>
+      <p class="lead">
+        {{ course.summary }}
+      </p>
+    </div>
+  </header>
+
+  <Card>
+    <div v-html="course.description"></div>
+    <a href="#">Ver más cursos de CSS</a>
+  </Card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import course from "@/data/course-detail.json";
+import Card from "@/components/Card.vue";
+
+export default defineComponent({
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      course,
+    };
+  },
+  computed: {
+    background(): string {
+      return `url(${this.course.background})`;
+    },
+  },
+});
+</script>
+
+<style scoped>
+.header {
+  min-height: 20rem;
+  margin-bottom: 5rem;
+  padding: 2rem 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+  color: #fff;
+  font-size: 1.3rem;
+}
+.header h1 {
+  line-height: 1;
+}
+.header-info {
+  max-width: 55ch;
+}
+small {
+  opacity: 0.8;
+}
+@media screen and (min-width: 100rem) {
+  .header {
+    background-image: none !important;
+  }
+}
+</style>
